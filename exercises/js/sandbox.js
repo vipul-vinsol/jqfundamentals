fe346d8c1ec5789b158ec75e96509aa96dc431d2
vipul-vinsol <vipul@vinsol.com>
2019-07-03 13:02:11 +0530

    section one completed

--- exercises/js/sandbox.js
+++ exercises/js/sandbox.js
@@ -18,3 +18,70 @@ $('img[alt]').length
 
 
 $('tr:odd')
+
+
+/*
+* Part Two
+*/
+
+$('img').each(() => {
+	console.log(this.attr('alt'));
+});
+
+
+
+
+$('input.input_text')
+.parent()
+.addClass('class');
+
+
+
+
+$('#myList > li.current').each(function(){
+	let item = $(this);
+	item.removeClass('current');
+	item.next().addClass('current');
+});
+
+
+
+
+$('#specials').find('input[type="submit"]')
+
+
+
+$('#slideshow > li:first-child')
+.addClass('current')
+.siblings()
+.addClass('disabled')
+
+/*
+* Part Three
+*/
+
+for(let i = 0; i < 5; i++)
+	$('#myList').append(`<li>List item ${ 8 + i }</li>`)
+
+
+
+$('#myList > li:odd').remove()
+
+
+
+
+$('div.module')
+.last()
+.append(`<h2>Heading added by jquery</h2>
+	<p>Paragrapgh added by jquery</p>`);
+
+
+
+
+$('select[name="day"]')
+.append(`<option value="wednesday">Wednesday</option>`);
+
+
+
+
+$('div.module').last().after('<div class="module"><img src="images/bread.jpg" alt="bread" /></div>') 
