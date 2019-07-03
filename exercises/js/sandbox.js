e060c8c92f078dfa7bff7dcd2c40f1145f030d57
vipul-vinsol <50447641+vipul-vinsol@users.noreply.github.com>
2019-07-03 17:36:24 +0530

    Update sandbox.js

--- exercises/js/sandbox.js
+++ exercises/js/sandbox.js
@@ -20,63 +20,63 @@ $('tr:odd')
 */
 
 $('img').each(() => {
-	console.log(this.attr('alt'));
+  console.log(this.attr('alt'));
 });
 
 
 
 
 $('input.input_text')
-.parent()
-.addClass('class');
+  .parent()
+  .addClass('class');
 
 
 
 
 $('#myList > li.current').each(function(){
-	let item = $(this);
-	item.removeClass('current');
-	item.next().addClass('current');
+  let item = $(this);
+  item.removeClass('current');
+  item.next().addClass('current');
 });
 
 
 
 
-$('#specials').find('input[type="submit"]')
+$('#specials').find('input[type="submit"]');
 
 
 
 $('#slideshow > li:first-child')
-.addClass('current')
-.siblings()
-.addClass('disabled')
+  .addClass('current')
+  .siblings()
+  .addClass('disabled');
 
 /*
 * Part Three
 */
 
-for(let i = 0; i < 5; i++)
-	$('#myList').append(`<li>List item ${ 8 + i }</li>`)
+for(let i = 0; i < 5; i++) {
+  $('#myList').append(`<li>List item ${ 8 + i }</li>`);
+}
 
 
-
-$('#myList > li:odd').remove()
-
-
+$('#myList > li:odd').remove();
 
 
 $('div.module')
-.last()
-.append(`<h2>Heading added by jquery</h2>
+  .last()
+  .append(`<h2>Heading added by jquery</h2>
 	<p>Paragrapgh added by jquery</p>`);
 
 
 
 
 $('select[name="day"]')
-.append(`<option value="wednesday">Wednesday</option>`);
+  .append(`<option value="wednesday">Wednesday</option>`);
 
 
 
 
-$('div.module').last().after('<div class="module"><img src="images/bread.jpg" alt="bread" /></div>') 
+$('div.module')
+  .last()
+  .after('<div class="module"><img src="images/bread.jpg" alt="bread" /></div>'); 
