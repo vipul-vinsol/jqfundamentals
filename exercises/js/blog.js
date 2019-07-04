e9c4b60139a8c00e53eeb0f5614b214fbc59458a
vipul-vinsol <50447641+vipul-vinsol@users.noreply.github.com>
2019-07-04 14:36:37 +0530

    Update blog.js

--- exercises/js/blog.js
+++ exercises/js/blog.js
@@ -2,7 +2,7 @@ let blogHeadings = $('#blog h3');
 
 // Solution
 blogHeadings.click(function(e) {
-	e.preventDefault();
-	$('#blog').find('.reading').removeClass('reading').slideUp();
-	$(this).siblings('p').addClass('reading').slideDown();
+  e.preventDefault();
+  $('#blog').find('.reading').removeClass('reading').slideUp();
+  $(this).siblings('p').addClass('reading').slideDown();
 });
