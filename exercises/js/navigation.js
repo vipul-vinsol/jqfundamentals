95c0c57544c1324363a5eb23d05594ded2e427f3
vipul-vinsol <vipul@vinsol.com>
2019-07-05 12:17:41 +0530

    Navigation in OO

--- exercises/js/navigation.js
+++ exercises/js/navigation.js
@@ -1,5 +1,26 @@
-$('#nav li').hover(function() {
-  $(this).find('ul').css('display', 'block');
-  }, function() {
-  $(this).find('ul').css('display', 'none');
-});
+class Navigation {
+	constructor() {
+		this.nav = $('#nav li');
+		this.mouseOn = this.mouseOn.bind();
+		this.mouseLeave = this.mouseLeave.bind();
+	}
+
+	init() {
+		this.bindEvents();
+	}
+
+	bindEvents() {
+		this.nav.hover(this.mouseOn, this.mouseLeave);		
+	}
+
+	mouseOn(e) {
+  		$(e.currentTarget).find('ul').css('display', 'block');
+	}
+
+	mouseLeave(e) {
+		$(e.currentTarget).find('ul').css('display', 'none');	
+	}
+}
+
+let navigation = new Navigation();
+navigation.init();
