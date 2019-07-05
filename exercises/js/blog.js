c9dde1bbc432ab216a6626a23a3b3a43577937e9
vipul-vinsol <vipul@vinsol.com>
2019-07-05 12:11:15 +0530

    Revel OO completed

--- exercises/js/blog.js
+++ exercises/js/blog.js
@@ -1,8 +1,23 @@
-let blogHeadings = $('#blog h3');
+class Blog {
+	constructor() {
+		this.blogHeadings = $('#blog h3');
+		this.handler = this.handler.bind();
+	}
 
-// Solution
-blogHeadings.click(function(e) {
-  e.preventDefault();
-  $('#blog').find('.reading').removeClass('reading').slideUp();
-  $(this).siblings('p').addClass('reading').slideDown();
-});
+	init() {
+		this.bindEvents();
+	}
+
+	bindEvents() {
+		this.blogHeadings.click(this.handler);		
+	}
+
+	handler(e) {
+		e.preventDefault();
+  		$('#blog').find('.reading').removeClass('reading').slideUp();
+  		$(e.currentTarget).siblings('p').addClass('reading').slideDown();		
+	}
+}
+
+let blog = new Blog();
+blog.init();
