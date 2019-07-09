6ca35a169bb6dd64f5ae27e3f3150d8b91cf4bb2
vipul-vinsol <50447641+vipul-vinsol@users.noreply.github.com>
2019-07-09 12:10:42 +0530

    Update slideshow.js

--- exercises/js/slideshow.js
+++ exercises/js/slideshow.js
@@ -1,33 +1,36 @@
 class SlideShow {
-  constructor() {
-    this.items = $('#slideshow li');
+  constructor(targetElemId, duration) {
+    this.slideShowDuration = duration;
+    this.targetElem = $(`#${targetElemId}`);
+    this.items = this.targetElem.children('li');
     this.itemCount = this.items.length;
   }
 
   init() {
     this.setup();
-    this.changeImagesEveryThreeSeconds();
+    this.changeImageOnSlideDuration();
   }
 
   setup() {
     // Move to Top
-    $('body').prepend($('#slideshow'));
+    $('body').prepend(this.targetElem);
 
     // Hide All except first
     $('#slideshow li:gt(0)').hide();
     
     // Create Stats Section
-    $('#slideshow').after(`
+    this.targetElem.after(`
       <h1>Slide Show Details</h1>
       <h2> Total Images :- ${this.itemCount} </h2>
       <h2> Current Image :- <span id='currentImageNumber'>1</span> </h2>
     `);
   }
 
-  changeImagesEveryThreeSeconds() {
+  changeImageOnSlideDuration() {
     let i = 0;
+    let showHideInterval = Math.floor(this.slideShowDuration / 3);
     setInterval(() => {
-      this.items.eq(i).fadeOut(1000, () => {
+      this.items.eq(i).fadeOut(showHideInterval, () => {
         // Increment the counter and roll around
         ++i;
         if(i === this.itemCount) i = 0;
@@ -36,12 +39,12 @@ class SlideShow {
         $('#currentImageNumber').text(i+1);
         
         // FadeIn the next Image
-        this.items.eq(i).fadeIn(1000);
+        this.items.eq(i).fadeIn(showHideInterval);
       })
-    }, 3000);
+    }, this.slideShowDuration);
   }
 }
 
 
-let slideshow = new SlideShow();
+let slideshow = new SlideShow('slideshow', 3000);
 slideshow.init();
